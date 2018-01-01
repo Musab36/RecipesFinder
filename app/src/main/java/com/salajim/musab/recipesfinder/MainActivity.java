7b82875f775385a9b22b8e53b02b6b1df9fa83a9
Musab36 <musabhd@gmail.com>
2018-01-02 02:37:36 +0300

    retrieving data from the api

--- app/src/main/java/com/salajim/musab/recipesfinder/MainActivity.java
+++ app/src/main/java/com/salajim/musab/recipesfinder/MainActivity.java
@@ -3,16 +3,38 @@ package com.salajim.musab.recipesfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    private RecipesAdapter mAdapter;
+    private ArrayList<Recipes> mRecipes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String recipe = intent.getStringExtra("recipe");
+
+        getRecipes(recipe);
     }
 
     // Menus inflated here
@@ -44,5 +66,31 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getRecipes(String recipes) {
+        final RecipesService recipesService = new RecipesService();
+        RecipesService.findRecipes(recipes, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mRecipes = recipesService.processResults(response);
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new RecipesAdapter(getApplicationContext(), mRecipes);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
     }
 }
